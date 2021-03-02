7b97c6ed71f11140ab0a6fbafa251d2758270143
Ludwing Perez <ludwing@ludwing-laptop>
2021-03-01 20:03:31 -0600

    Crear middleware de autenticación de token.

---
+++ api/components/user/secure.js
@@ -0,0 +1,20 @@
+// exportar un middleware
+const auth = require('../../../auth');
+
+module.exports = function checkAuth(action) {
+  // Middleware que hace el check del token
+
+  function middleware(req, res, next) {
+    switch(action) {
+      case 'update':
+        // lógica
+        const owner = req.body.id;
+        auth.check.own(req, owner);
+        break;
+      default:
+        next();
+    }
+  }
+
+  return middleware;
+}
--- auth/index.js
+++ auth/index.js
@@ -1,7 +1,49 @@
 const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+const secret = config.jwt.secret;
 
 function sign(data) {
-  return jwt.sign(data, 'SECRET!');
+  return jwt.sign(data, secret);
+}
+
+function verify(token) {
+  return jwt.verify(token, secret)
+}
+
+const check = {
+  own: function(req, owner) {
+    const decodedToken = decodeHeader(req);
+    console.log(decodedToken);
+  }
+}
+
+function getToken(auth) {
+  // La función descompone el encabezado Authorization y retorna únicamente el
+  // token JWT.  El formato del header es 'Bearer asd...sfs.asdf...asdf.as...df'
+
+  if (!auth) {
+    throw new Error('No viene token');
+  }
+
+  if (auth.indexOf('Bearer ') === -1) {
+    throw new Error('Formato inválido');
+  } 
+
+  let token = auth.replace('Bearer ', '');
+
+  return token;
+}
+
+function decodeHeader(req) {
+  // Procesa la request para obtener el usuario en base al token recibido
+  const authorization = req.headers.authorization || '';
+  const token = getToken(authorization);
+  const decoded = verify(token);
+
+  req.user = decoded;
+
+  return decoded;
 }
 
 module.exports = {
