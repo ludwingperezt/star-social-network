f2ffedc2aef5d14ddbfca099531b73d59f886e7d
Ludwing Perez <mirage232@gmail.com>
2023-01-26 12:00:56 -0600

    Mediante variables de entorno configurar cómo acceder a la base de datos.

--- api/components/user/index.js
+++ api/components/user/index.js
@@ -3,9 +3,22 @@
  * inyectar el componente de almacenamiento.
  */
 
+const config = require('../../../config');
 const controller = require('./controller');
-//const store = require('../../../store/postgres');
-const store = require('../../../store/remote-postgres');
+
+// En este punto mediante variables de entorno se puede
+// determinar qué tipo de acceso a la base de datos se utiliza.
+// Si en la configuración se pone que remoteDB sea false, entonces el acceso
+// a la db será a través del componente de la base de datos.
+// Si esa variable es true, entonces el acceso a la db se haría a través del
+// microservicio API de acceso a la base de datos.
+let store;
+if (config.remoteDB === true) {
+    store = require('../../../store/postgres');    
+}
+else {
+    store = require('../../../store/remote-postgres');
+}
 
 // Se inyecta el repositorio al controlador
 module.exports = controller(store);
